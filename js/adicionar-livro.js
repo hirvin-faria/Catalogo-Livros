bd0e146f9b272f27a98d276dc072e42034d38815
Hirvin Faria <hirvin.nogueira.faria@gmail.com>
2019-09-30 11:36:45 -0300

    Criacao da funcao adicionar novo livro

---
+++ js/adicionar-livro.js
@@ -0,0 +1,43 @@
+var botaoAdicionar = document.querySelector("#adicionar-livro")
+console.log(botaoAdicionar)
+
+botaoAdicionar.addEventListener("click", function(){
+    event.preventDefault()
+
+    var formulario = document.querySelector("#formulario-adicionar-livro")
+    console.log(formulario)
+
+    var livro = {
+        titulo: formulario.titulo.value,
+        genero: formulario.genero.value,
+        editora: formulario.editora.value,
+        avaliacao: formulario.avaliacao.value
+    }
+
+    console.log(livro)
+
+    var livroTr = document.createElement("tr")
+
+    var tituloTd = document.createElement("td")
+    var generoTd = document.createElement("td")
+    var editoraTd = document.createElement("td")
+    var avaliacaoTd = document.createElement("td")
+
+    tituloTd.textContent = livro.titulo
+    generoTd.textContent = livro.genero
+    editoraTd.textContent = livro.editora
+    avaliacaoTd.textContent = livro.avaliacao
+
+    livroTr.appendChild(tituloTd)
+    livroTr.appendChild(generoTd)
+    livroTr.appendChild(editoraTd)
+    livroTr.appendChild(avaliacaoTd)
+
+    console.log(livroTr)
+
+    var tabelaLivros = document.querySelector("#lista-livros")
+    tabelaLivros.appendChild(livroTr)
+
+    formulario.reset()
+
+})
